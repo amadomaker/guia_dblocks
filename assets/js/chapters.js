a00515cdc09a2f4a03656c5d1d6b81dd86072231
Antonio Mortari <antoniomortari09@gmail.com>
2025-02-21 00:01:06 -0300

    feat: adicionado js para páginas de capítulos

---
+++ assets/js/chapters.js
@@ -0,0 +1,50 @@
+const IMAGE_PATH = "../assets/images";
+const DATA_URL = "../data/cards.json";
+
+const currentChapterUrl = window.location.pathname.substring(1);
+
+const loadDomChapters = async () => {
+  try {
+    const response = await fetch(DATA_URL);
+    if (!response.ok) throw new Error("Erro ao carregar os dados.");
+
+    const chaptersData = await response.json();
+    const containerChapters = document.getElementById("chapters-list");
+
+    if (!containerChapters) {
+      console.error("Elemento #chapters-list não encontrado.");
+      return;
+    }
+
+    containerChapters.innerHTML = "";
+
+    chaptersData.forEach((cardData, index) => {
+      const chapterNumber = index + 1;
+      
+      const cardLink = document.createElement("a");
+      cardLink.href = window.location.origin + "/" + cardData.url;
+      cardLink.classList.add("chapter");
+
+      if (cardData.url && cardData.url === currentChapterUrl) {
+        cardLink.classList.add("selected");
+      }
+
+      cardLink.innerHTML = `
+        <img src="${IMAGE_PATH}/${cardData.image}" alt="Capítulo ${chapterNumber}" class="card-image" />
+        <div class="chapter-content">
+          <h3 class="chapter-title">${cardData.title}</h3>
+        </div>
+      `;
+
+      containerChapters.appendChild(cardLink);
+    });
+  } catch (error) {
+    console.error("Erro ao carregar os capítulos:", error);
+  }
+};
+
+function truncateText(text, maxLength = 20) {
+  return text.length > maxLength ? text.substring(0, maxLength) + "..." : text;
+}
+
+window.addEventListener("DOMContentLoaded", loadDomChapters);
